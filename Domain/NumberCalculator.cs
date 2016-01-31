5123076506e0e618afb790a9f7336a88a3d32638
konzab <sean.allenco@gmail.com>
2016-01-31 19:30:34 +0200

    couple of changes test

---
+++ Domain/NumberCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class NumberCalculator
+    {
+        int num1 = 0;
+        int num2 = 0;
+        public NumberCalculator(int number1, int number2)
+        {
+            num1 = number1;
+            num2 = number2;
+        }
+        /// <summary>
+        /// Add two numbers passed into the class
+        /// </summary>
+        /// <returns>int</returns>
+        public int AddNumber()
+        {
+            return num1 + num2;
+        }
+    }
+}
